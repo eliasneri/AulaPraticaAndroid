f9aa2aa0bd543e5aaab9f6a6b1aecfc986d4f7b3
eliasneri <eliasneri@hotmail.com>
2021-11-16 17:51:46 -0300

    Projeto Criado

---
+++ app/src/main/java/com/eliasneri/aulapraticaandroid/GasolinaEtanol.java
@@ -0,0 +1,48 @@
+package com.eliasneri.aulapraticaandroid;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.eliasneri.aulapraticaandroid.databinding.ActivityGasolinaEtanalBinding;
+
+public class GasolinaEtanol extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityGasolinaEtanalBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityGasolinaEtanalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_gasolina_etanal);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_gasolina_etanal);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
